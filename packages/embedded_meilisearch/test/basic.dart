ee241d7f792f743ffd673890f500933e9b57578b
Gregory Conrad <gregorysconrad@gmail.com>
2022-11-07 10:44:20 -0500

    Added basic first integ test

---
+++ packages/embedded_meilisearch/test/basic.dart
@@ -0,0 +1,54 @@
+import 'dart:ffi';
+import 'dart:io';
+
+import 'package:embedded_meilisearch/embedded_meilisearch.dart';
+import 'package:test/test.dart';
+
+void main() {
+  final instanceDir = Directory('').createTempSync('testInstanceTemp-');
+  test('Basic integration test with movies', () async {
+    // TODO the dylibPath might be .so in CI/linux. might need to do Platform.isX
+    const dylibPath = 'native/target/debug/libembedded_milli.dylib';
+
+    final milli = Meili.createWrapper(DynamicLibrary.open(dylibPath));
+    final instance = await Meili.getInstance(
+      path: instanceDir.path,
+      milli: milli,
+    );
+    final index = await instance.getIndex('movies');
+
+    final allDocs = [
+      {
+        "id": 2,
+        "title": "Prideand Prejudice",
+        "author": "Jane Austin",
+        "genre": "romance",
+        "price": "3.5",
+      },
+      {
+        "id": 456,
+        "title": "Le Petit Prince",
+        "author": "Antoine de Saint-Exupéry",
+        "genre": "adventure",
+        "price": "10.0",
+      },
+      {
+        "id": 1,
+        "title": "Wonderland",
+        "author": "Lewis Carroll",
+        "genre": "fantasy",
+        "price": "25.99",
+      },
+      {
+        "id": 4,
+        "title": "Harry Potter ing fantasy\0lood Prince",
+        "author": "J. K. Rowling",
+        "genre": "fantasy\0",
+      },
+    ];
+    await index.addDocuments(allDocs);
+    final foundDocs = await index.search('horry');
+    assert(foundDocs == [allDocs[3]], 'Search for horry should return harry');
+  });
+  instanceDir.deleteSync();
+}
