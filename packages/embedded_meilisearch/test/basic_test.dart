5341708063edc1dd29773d9cada2a9d550cb0b1c
Gregory Conrad <gregorysconrad@gmail.com>
2022-11-07 17:26:54 -0500

    Added better tests

--- packages/embedded_meilisearch/test/basic_test.dart
+++ packages/embedded_meilisearch/test/basic_test.dart
@@ -1,61 +1,51 @@
-import 'dart:ffi';
 import 'dart:io';
 
 import 'package:embedded_meilisearch/embedded_meilisearch.dart';
 import 'package:test/test.dart';
 
-void main() {
-  const libName = 'embedded_milli';
-  final libPrefix = {
-    Platform.isWindows: '',
-    Platform.isMacOS: 'lib',
-    Platform.isLinux: 'lib',
-  }[true]!;
-  final libSuffix = {
-    Platform.isWindows: 'dll',
-    Platform.isMacOS: 'dylib',
-    Platform.isLinux: 'so',
-  }[true]!;
-  final dylibPath = 'native/target/debug/$libPrefix$libName.$libSuffix';
-  final milli = Meili.createWrapper(DynamicLibrary.open(dylibPath));
-  final instanceDir = Directory.systemTemp.createTempSync('testInstanceTemp-');
+import 'utils.dart';
 
-  test('Basic integration test with movies', () async {
-    final instance = Meili.getInstance(
-      path: instanceDir.path,
-      milli: milli,
-    );
+const allDocs = [
+  {
+    "id": 2,
+    "title": "Prideand Prejudice",
+    "author": "Jane Austin",
+    "genre": "romance",
+    "price": "3.5",
+  },
+  {
+    "id": 456,
+    "title": "Le Petit Prince",
+    "author": "Antoine de Saint-Exupéry",
+    "genre": "adventure",
+    "price": "10.0",
+  },
+  {
+    "id": 1,
+    "title": "Wonderland",
+    "author": "Lewis Carroll",
+    "genre": "fantasy",
+    "price": "25.99",
+  },
+  {
+    "id": 4,
+    "title": "Harry Potter ing fantasy\0lood Prince",
+    "author": "J. K. Rowling",
+    "genre": "fantasy\0",
+  },
+];
+
+void main() {
+  late Directory dir;
+  setUp(() => dir = createTmpDir());
+  tearDown(() => dir.deleteSync(recursive: true));
+
+  test('Basic functionality', () async {
+    // Test data taken from the example at:
+    // https://github.com/meilisearch/milli/blob/main/README.md
+    final instance = Meili.getInstance(path: dir.path, milli: milli);
     final index = instance.getIndex('movies');
 
-    final allDocs = [
-      {
-        "id": 2,
-        "title": "Prideand Prejudice",
-        "author": "Jane Austin",
-        "genre": "romance",
-        "price": "3.5",
-      },
-      {
-        "id": 456,
-        "title": "Le Petit Prince",
-        "author": "Antoine de Saint-Exupéry",
-        "genre": "adventure",
-        "price": "10.0",
-      },
-      {
-        "id": 1,
-        "title": "Wonderland",
-        "author": "Lewis Carroll",
-        "genre": "fantasy",
-        "price": "25.99",
-      },
-      {
-        "id": 4,
-        "title": "Harry Potter ing fantasy\0lood Prince",
-        "author": "J. K. Rowling",
-        "genre": "fantasy\0",
-      },
-    ];
     await index.addDocuments(allDocs);
     final foundDocs = await index.search('horry');
     expect(
@@ -64,6 +54,4 @@ void main() {
       reason: 'Search for horry should only return harry',
     );
   });
-
-  instanceDir.deleteSync();
 }
---
+++ packages/embedded_meilisearch/test/movie_test.dart
@@ -0,0 +1,10 @@
+import 'package:test/test.dart';
+
+const url =
+    'https://raw.githubusercontent.com/prust/wikipedia-movie-data/master/movies.json';
+
+void main() {
+  test('', () {
+    // TODO test all parts of the package
+  });
+}
