b5369a738aac37873f4191a645258a2b31543d9b
Gregory Conrad <gregorysconrad@gmail.com>
2022-11-18 11:24:57 -0500

    Prettified example

--- packages/flutter_mimir/example/lib/main.dart
+++ packages/flutter_mimir/example/lib/main.dart
@@ -22,25 +22,12 @@ void main() async {
 
   // Finally, run our application
   runApp(ProviderScope(
-    overrides: [
-      indexProvider.overrideWith((_) => index),
-    ],
+    overrides: [indexProvider.overrideWith((_) => index)],
     child: MaterialApp(
       title: 'Mimir Demo',
-      home: Builder(builder: (context) {
-        return Scaffold(
-          appBar: AppBar(
-            title: const Text('Mimir Demo'),
-            actions: [
-              IconButton(
-                icon: const Icon(Icons.info),
-                onPressed: () => showInfoDialog(context),
-              ),
-            ],
-          ),
-          body: const Body(),
-        );
-      }),
+      theme: ThemeData.light(useMaterial3: true),
+      darkTheme: ThemeData.dark(useMaterial3: true),
+      home: const Body(),
     ),
   ));
 }
@@ -66,32 +53,56 @@ class Body extends HookConsumerWidget {
     final textController = useTextEditingController();
     final searchResults = ref.watch(searchProvider);
 
-    return Column(children: [
-      TextField(
-        controller: textController,
-        onChanged: (q) => ref.read(queryProvider.notifier).state = q,
-        decoration: InputDecoration(
-          prefixIcon: const Icon(Icons.search),
-          suffixIcon: IconButton(
-            icon: const Icon(Icons.cancel),
-            onPressed: () {
-              textController.text = '';
-              ref.read(queryProvider.notifier).state = '';
-            },
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Mimir Demo'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.info),
+            onPressed: () => showInfoDialog(context),
+          ),
+        ],
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(56),
+          child: Padding(
+            padding: const EdgeInsets.only(left: 8, right: 8, bottom: 8),
+            child: TextField(
+              controller: textController,
+              onChanged: (q) => ref.read(queryProvider.notifier).state = q,
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.zero,
+                border: const OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(100)),
+                ),
+                prefixIcon: const Icon(Icons.search),
+                suffixIcon: IconButton(
+                  icon: const Icon(Icons.cancel),
+                  onPressed: () {
+                    textController.text = '';
+                    ref.read(queryProvider.notifier).state = '';
+                  },
+                ),
+              ),
+            ),
           ),
         ),
       ),
-      Expanded(
-        child: searchResults.when(
-          data: (searchResults) => ListView.builder(
-            itemCount: searchResults.length,
-            itemBuilder: (_, index) => MovieCard(movie: searchResults[index]),
+      body: Column(children: [
+        Expanded(
+          child: searchResults.when(
+            data: (searchResults) => ListView.builder(
+              padding: const EdgeInsets.only(top: 16, bottom: 8),
+              itemCount: searchResults.length,
+              itemBuilder: (_, index) => MovieCard(movie: searchResults[index]),
+            ),
+            error: (e, s) => Center(child: Text('Error: $e')),
+            loading: () => const Center(
+              child: CircularProgressIndicator.adaptive(),
+            ),
           ),
-          error: (e, s) => Center(child: Text('Error: $e')),
-          loading: CircularProgressIndicator.adaptive,
         ),
-      ),
-    ]);
+      ]),
+    );
   }
 }
 
@@ -103,20 +114,50 @@ class MovieCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: ListTile(
-        leading: Image.network(
-          'https://www.themoviedb.org/t/p/w45${movie['poster_path']}',
-        ),
-        trailing: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [Text(movie['release_date'])],
-        ),
-        title: Text(movie['title']),
-        subtitle: Text(
-          movie['overview'],
-          maxLines: 3,
-          overflow: TextOverflow.ellipsis,
-        ),
+      clipBehavior: Clip.antiAlias,
+      margin: const EdgeInsets.only(left: 16, right: 16, bottom: 8),
+      child: ConstrainedBox(
+        constraints: const BoxConstraints(maxHeight: 100),
+        child: Row(children: [
+          AspectRatio(
+            aspectRatio: 188.0 / 282.0,
+            child: Image.network(
+              'https://www.themoviedb.org/t/p/w188_and_h282_bestv2${movie['poster_path']}',
+              fit: BoxFit.fill,
+              errorBuilder: (_, __, ___) => Center(
+                child: Icon(
+                  Icons.cancel,
+                  color: Theme.of(context).colorScheme.error,
+                ),
+              ),
+              loadingBuilder: (_, child, progress) {
+                if (progress == null) return child;
+                return const Center(
+                  child: CircularProgressIndicator.adaptive(),
+                );
+              },
+            ),
+          ),
+          const SizedBox(width: 8),
+          Expanded(
+            child: ListTile(
+              contentPadding: EdgeInsets.zero,
+              title: Text(
+                movie['title'],
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
+              subtitle: Text(
+                movie['overview'],
+                maxLines: 3,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+          ),
+          const SizedBox(width: 8),
+          Text((movie['release_date'] as String).split('-')[0]),
+          const SizedBox(width: 12),
+        ]),
       ),
     );
   }
