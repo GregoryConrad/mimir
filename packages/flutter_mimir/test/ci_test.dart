9a5a99245967b1f84640468684e536b328dc5979
Gregory Conrad <gregorysconrad@gmail.com>
2022-11-21 13:00:53 -0500

    chore(flutter_mimir): updated comments in a few files

--- packages/flutter_mimir/test/ci_test.dart
+++ packages/flutter_mimir/test/ci_test.dart
@@ -2,6 +2,9 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('Dummy test for ci', () {
+    // At least as of now, no true easily-testable features are in
+    // flutter_mimir. It is mostly just ffi wrappers/convenience.
+    // Thus, we leave testing up to mimir and the flutter_example.
     expect(true, true);
   });
 }
