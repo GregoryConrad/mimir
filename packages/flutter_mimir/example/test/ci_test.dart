ce8be7ec9a0dac83f6cbc48ea2fd50a336bc1a2a
Gregory Conrad <gregorysconrad@gmail.com>
2022-11-30 15:27:38 -0500

    ci: fixes for flutter ci tests

---
+++ packages/flutter_mimir/example/test/ci_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('Dummy test for ci', () {
+    // All true testing takes place over in integration_test/
+    expect(true, true);
+  });
+}
