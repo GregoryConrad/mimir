eab5fcdbbfba201bd5b0712156936f14c8e263fc
Gregory Conrad <gregorysconrad@gmail.com>
2022-12-06 14:30:15 -0500

    feat: switch android build over to CMake to enable CI testing

--- scripts/version.sh
+++ scripts/version.sh
@@ -2,25 +2,18 @@
 
 CURR_VERSION=mimir-v`awk '/^version: /{print $2}' packages/mimir/pubspec.yaml`
 
-# Android
-ANDROID_HEADER="def releaseTagVersion = '$CURR_VERSION' \/\/ generated; do not edit"
-sed -i.bak "1 s/.*/$ANDROID_HEADER/" packages/flutter_mimir/android/build.gradle
-rm packages/flutter_mimir/android/*.bak
-
 # iOS & macOS
 APPLE_HEADER="release_tag_name = '$CURR_VERSION' # generated; do not edit"
 sed -i.bak "1 s/.*/$APPLE_HEADER/" packages/flutter_mimir/ios/flutter_mimir.podspec
 sed -i.bak "1 s/.*/$APPLE_HEADER/" packages/flutter_mimir/macos/flutter_mimir.podspec
 rm packages/flutter_mimir/macos/*.bak packages/flutter_mimir/ios/*.bak
 
-# Linux
-LINUX_HEADER="set(MimirVersion \"$CURR_VERSION\") # generated; do not edit"
-sed -i.bak "1 s/.*/$LINUX_HEADER/" packages/flutter_mimir/linux/CMakeLists.txt
-rm packages/flutter_mimir/linux/*.bak
-
-# Windows
-WINDOWS_HEADER="set(MimirVersion \"$CURR_VERSION\") # generated; do not edit"
-sed -i.bak "1 s/.*/$WINDOWS_HEADER/" packages/flutter_mimir/windows/CMakeLists.txt
-rm packages/flutter_mimir/windows/*.bak
+# CMake platforms (Linux, Windows, and Android)
+CMAKE_HEADER="set(MimirVersion \"$CURR_VERSION\") # generated; do not edit"
+for CMAKE_PLATFORM in android linux windows
+do
+    sed -i.bak "1 s/.*/$CMAKE_HEADER/" packages/flutter_mimir/$CMAKE_PLATFORM/CMakeLists.txt
+    rm packages/flutter_mimir/$CMAKE_PLATFORM/*.bak
+done
 
 git add packages/flutter_mimir/
