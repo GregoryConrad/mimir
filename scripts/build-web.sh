32661b835970874fb127a2d551498b3ef08aa81f
Gregory Conrad <gregorysconrad@gmail.com>
2023-01-21 15:02:36 -0500

    fix: get lmdb compiling

--- scripts/build-web.sh
+++ scripts/build-web.sh
@@ -13,13 +13,13 @@ git clone --recursive https://github.com/WebAssembly/wasi-libc $WASI_LIBC_ROOT
 (
     cd $WASI_LIBC_ROOT &&
     git checkout $WASI_VERSION_TAG &&
-    make CC=$LLVM_BIN/clang AR=$LLVM_BIN/llvm-ar NM=$LLVM_BIN/llvm-nm
+    make CC=$LLVM_BIN/clang AR=$LLVM_BIN/llvm-ar NM=$LLVM_BIN/llvm-nm THREAD_MODEL=posix
 )
 
 # Configure environment variables for cargo build
 WASI_LIBC_ROOT=`realpath $WASI_LIBC_ROOT`
-export CC="$LLVM_BIN/clang -D_WASI_EMULATED_MMAN --sysroot=$WASI_LIBC_ROOT/sysroot"
-export LLD="$LLVM_BIN/lld -lwasi-emulated-mman"
+export CC="$LLVM_BIN/clang -D_WASI_EMULATED_MMAN -D_WASI_EMULATED_SIGNAL -D_WASI_EMULATED_GETPID --sysroot=$WASI_LIBC_ROOT/sysroot"
+export LLD="$LLVM_BIN/lld -lwasi-emulated-mman -lwasi-emulated-signal -lwasi-emulated-getpid"
 export LD="$LLD"
 export AR="$LLVM_BIN/llvm-ar"
 export NM="$LLVM_BIN/llvm-nm"
@@ -27,5 +27,5 @@ export NM="$LLVM_BIN/llvm-nm"
 # Build for web
 TARGET=wasm32-wasi
 rustup target add $TARGET
-cargo build -r --target=$TARGET
+cargo build -r --target=$TARGET --features posix-sem
 cp ../target/wasm32-wasi/release/embedded_milli.wasm .
