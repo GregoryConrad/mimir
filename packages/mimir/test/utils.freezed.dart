28819ef046a889321ad434cb0317448c72f6f985
MimirActionsBot <mimiractionsbot@users.noreply.github.com>
2023-12-18 08:08:52 +0000

    chore: update generated files

--- packages/mimir/test/utils.freezed.dart
+++ packages/mimir/test/utils.freezed.dart
@@ -332,7 +332,7 @@ class _$ComparableMimirIndexSettingsImpl
   }
 
   @override
-  bool operator ==(dynamic other) {
+  bool operator ==(Object other) {
     return identical(this, other) ||
         (other.runtimeType == runtimeType &&
             other is _$ComparableMimirIndexSettingsImpl &&
