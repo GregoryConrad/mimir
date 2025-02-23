3ebad5dd5755b0ac060b5905419d89f3adaca40e
Gregory Conrad <gregorysconrad@gmail.com>
2025-02-22 23:56:34 -0500

    chore: add nix integration

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "Mimir";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+      fenix,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        formatter = pkgs.nixfmt-rfc-style;
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            # Switch "stable" to "complete" for nightly
+            fenix.packages.${system}.stable.toolchain
+
+            flutter
+            melos
+            zig
+            llvm
+          ];
+
+          env = {
+            RUST_BACKTRACE = "1";
+          };
+        };
+      }
+    );
+}
